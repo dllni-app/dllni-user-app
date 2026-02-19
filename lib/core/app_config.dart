d0c022cdf50b73815b4fd3c934a1f3beb2aec781
Ahmad_M_Albetar <zaza66108@gmail.com>
2026-02-19 12:08:10 +0300

    make app ready

---
+++ lib/core/app_config.dart
@@ -0,0 +1,7 @@
+class AppConfig {
+  const AppConfig._();
+
+  static const String appName = 'دللني';
+  static const String orgIdentifier = 'com.dllni.userapp';
+  static const String baseUrl = 'https:&#x2F;&#x2F;dllni.com';
+}
