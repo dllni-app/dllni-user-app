38c0758f0d921f28d6f8bd5a9430513c0a025c7d
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-19 00:22:34 +0300

    Add customer user cache key

--- lib/core/session/user_session_keys.dart
+++ lib/core/session/user_session_keys.dart
@@ -6,4 +6,5 @@ abstract final class UserSessionKeys {
   static const String customerPhone = 'customer_phone';
   static const String customerAvatarUrl = 'customer_avatar_url';
   static const String customerPhoneVerifiedAt = 'customer_phone_verified_at';
+  static const String loggedInUser = 'logged_in_user';
 }
