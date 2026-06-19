4f96caeca0f1f2253da0634c7db6b0b814b2a087
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-19 23:31:51 +0300

    Make login help screen wrapper widget

--- lib/features/auth/view/screens/login_help_screen.dart
+++ lib/features/auth/view/screens/login_help_screen.dart
@@ -1,5 +1,12 @@
+import 'package:flutter/material.dart';
+
 import 'account_recovery_screen.dart';
 
-class LoginHelpScreen extends AccountRecoveryScreen {
+class LoginHelpScreen extends StatelessWidget {
   const LoginHelpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AccountRecoveryScreen();
+  }
 }
