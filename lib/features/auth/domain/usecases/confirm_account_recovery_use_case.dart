933d5cc7ce2a771577e6439d5ee5c24946916ca7
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-19 21:44:07 +0300

    Add account recovery confirm use case

---
+++ lib/features/auth/domain/usecases/confirm_account_recovery_use_case.dart
@@ -0,0 +1,18 @@
+import 'package:common_package/helpers/typedef.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../data/models/auth_action_response_model.dart';
+import '../repository/auth_repo.dart';
+import 'auth_phone_params.dart';
+
+@lazySingleton
+class ConfirmAccountRecoveryUseCase implements UseCase<AuthActionResponseModel, ResetPasswordConfirmParams> {
+  final AuthRepo authRepo;
+
+  ConfirmAccountRecoveryUseCase({required this.authRepo});
+
+  @override
+  DataResponse<AuthActionResponseModel> call(ResetPasswordConfirmParams params) {
+    return authRepo.confirmAccountRecovery(params);
+  }
+}
