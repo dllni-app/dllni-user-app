8b8bc7705f9f7ad04a82f25b6df39d820cf7208d
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-19 21:43:58 +0300

    Add account recovery request use case

---
+++ lib/features/auth/domain/usecases/request_account_recovery_use_case.dart
@@ -0,0 +1,18 @@
+import 'package:common_package/helpers/typedef.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../data/models/auth_action_response_model.dart';
+import '../repository/auth_repo.dart';
+import 'auth_phone_params.dart';
+
+@lazySingleton
+class RequestAccountRecoveryUseCase implements UseCase<AuthActionResponseModel, AuthPhoneParams> {
+  final AuthRepo authRepo;
+
+  RequestAccountRecoveryUseCase({required this.authRepo});
+
+  @override
+  DataResponse<AuthActionResponseModel> call(AuthPhoneParams params) {
+    return authRepo.requestAccountRecovery(params);
+  }
+}
