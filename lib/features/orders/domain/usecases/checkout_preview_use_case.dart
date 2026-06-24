88552384b762f972dd1fb24f6bcb485e4ee07ea9
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-24 19:16:33 +0300

    Add checkout preview use case classes

--- lib/features/orders/domain/usecases/checkout_preview_use_case.dart
+++ lib/features/orders/domain/usecases/checkout_preview_use_case.dart
@@ -1,1 +1,31 @@
-export '../../data/models/merchant_cart_models.dart';
+import 'package:common_package/helpers/typedef.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../data/models/merchant_cart_models.dart';
+import '../repository/orders_repo.dart';
+
+@lazySingleton
+class PreviewRestaurantCheckoutUseCase
+    implements UseCase<CheckoutPreviewModel, CheckoutPreviewParams> {
+  final OrdersRepo ordersRepo;
+
+  PreviewRestaurantCheckoutUseCase({required this.ordersRepo});
+
+  @override
+  DataResponse<CheckoutPreviewModel> call(CheckoutPreviewParams params) {
+    return ordersRepo.previewRestaurantCheckout(params);
+  }
+}
+
+@lazySingleton
+class PreviewStoreCheckoutUseCase
+    implements UseCase<CheckoutPreviewModel, CheckoutPreviewParams> {
+  final OrdersRepo ordersRepo;
+
+  PreviewStoreCheckoutUseCase({required this.ordersRepo});
+
+  @override
+  DataResponse<CheckoutPreviewModel> call(CheckoutPreviewParams params) {
+    return ordersRepo.previewStoreCheckout(params);
+  }
+}
