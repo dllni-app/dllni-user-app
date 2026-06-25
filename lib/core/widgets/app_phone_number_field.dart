cede634c6ca75be00a1c1c5fccab4097b533d58e
Mohammed <mohjalab9@gamil.com>
2026-06-25 23:30:29 +0300

    edit syria flag for whole of the app

--- lib/core/widgets/app_phone_number_field.dart
+++ lib/core/widgets/app_phone_number_field.dart
@@ -1,5 +1,6 @@
 import 'package:common_package/common_package.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
 import '../helpers/phone_number_helper.dart';
@@ -7,6 +8,16 @@ import '../helpers/phone_number_helper.dart';
 enum AppPhoneFieldVariant { auth, profile }
 
 class AppPhoneNumberField extends StatefulWidget {
+  final String? label;
+
+  final bool isRequired;
+  final String hintText;
+  final PhoneNumber? initialValue;
+  final ValueChanged<PhoneNumber>? onChanged;
+  final bool enabled;
+  final AppPhoneFieldVariant variant;
+  final bool showLabel;
+  final Future<String?> Function(PhoneNumber?)? validator;
   const AppPhoneNumberField({
     super.key,
     this.label,
@@ -20,16 +31,6 @@ class AppPhoneNumberField extends StatefulWidget {
     this.validator,
   });
 
-  final String? label;
-  final bool isRequired;
-  final String hintText;
-  final PhoneNumber? initialValue;
-  final ValueChanged<PhoneNumber>? onChanged;
-  final bool enabled;
-  final AppPhoneFieldVariant variant;
-  final bool showLabel;
-  final Future<String?> Function(PhoneNumber?)? validator;
-
   @override
   State<AppPhoneNumberField> createState() => AppPhoneNumberFieldState();
 }
@@ -39,17 +40,50 @@ class AppPhoneNumberFieldState extends State<AppPhoneNumberField> {
 
   PhoneNumber? get phone => _phone;
 
-  Future<String?> validate() async {
-    if (widget.validator != null) {
-      return widget.validator!(_phone);
-    }
-    return validatePhoneNumber(_phone);
-  }
-
   @override
-  void initState() {
-    super.initState();
-    _phone = widget.initialValue;
+  Widget build(BuildContext context) {
+    final input = Row(
+      children: [
+        Expanded(
+          child: TextFormField(
+            decoration: _decoration(context),
+            initialValue: _phone?.phoneNumber,
+            onChanged: (value) {
+              setState(() {
+                _phone = PhoneNumber(phoneNumber: "+963$value");
+              });
+            },
+          ),
+        ),
+        SizedBox(width: 8),
+        _buildFlagsButton(),
+      ],
+    );
+
+    final field = widget.enabled ? input : AbsorbPointer(child: input);
+
+    if (!widget.showLabel || widget.label == null) {
+      return field;
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            AppText.bodyMedium(widget.label!, fontWeight: FontWeight.w500),
+            if (widget.isRequired)
+              AppText.bodyMedium(
+                '*',
+                color: context.error,
+                fontWeight: FontWeight.w500,
+              ),
+          ],
+        ),
+        const SizedBox(height: 8),
+        field,
+      ],
+    );
   }
 
   @override
@@ -58,6 +92,84 @@ class AppPhoneNumberFieldState extends State<AppPhoneNumberField> {
     if (widget.initialValue != oldWidget.initialValue) {
       _phone = widget.initialValue;
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _phone = widget.initialValue;
+  }
+
+  Future<String?> validate() async {
+    if (widget.validator != null) {
+      return widget.validator!(_phone);
+    }
+    return validatePhoneNumber(_phone);
+  }
+
+  Widget _buildFlagsButton() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 12),
+      clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(
+        color: const Color(0xffF9FAFB),
+        borderRadius: BorderRadius.circular(14),
+        border: Border.all(color: const Color(0xffE5E7EB), width: 1),
+      ),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(14),
+        // onTap: widget.enabled ? _changeCountry : null,
+        onTap: null,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(width: 4),
+            FittedBox(
+              child: Text(
+                '+963',
+                textDirection: TextDirection.ltr,
+                style: const TextStyle(
+                  color: Color(0xff2F2B3D),
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            const SizedBox(width: 8),
+            _buildFlagWidget(),
+            const SizedBox(width: 4),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFlagWidget() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 4),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(6),
+        border: Border.all(color: Colors.grey.shade300),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.08),
+            blurRadius: 6,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(4),
+        child: SvgPicture.asset(
+          'assets/images/svgs/sy.svg',
+          width: 28,
+          height: 20,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
 
   InputDecoration _decoration(BuildContext context) {
@@ -71,7 +183,10 @@ class AppPhoneNumberFieldState extends State<AppPhoneNumberField> {
         hintStyle: TextStyle(color: Colors.grey.shade500, fontSize: 14),
         filled: true,
         fillColor: fillColor,
-        contentPadding: const EdgeInsets.symmetric(horizontal: 14, vertical: 12),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 14,
+          vertical: 12,
+        ),
         prefixIcon: const Icon(Icons.phone_rounded, color: iconGray, size: 22),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(14),
@@ -102,7 +217,11 @@ class AppPhoneNumberFieldState extends State<AppPhoneNumberField> {
       filled: true,
       fillColor: fillColor,
       contentPadding: const EdgeInsets.symmetric(horizontal: 14, vertical: 12),
-      prefixIcon: const Icon(Icons.phone_rounded, color: Color(0xff15803D), size: 22),
+      prefixIcon: const Icon(
+        Icons.phone_rounded,
+        color: Color(0xff15803D),
+        size: 22,
+      ),
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(14),
         borderSide: const BorderSide(color: borderColor, width: 1),
@@ -123,73 +242,6 @@ class AppPhoneNumberFieldState extends State<AppPhoneNumberField> {
         borderRadius: BorderRadius.circular(14),
         borderSide: BorderSide(color: context.error, width: 1.2),
       ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final input = Directionality(
-      textDirection: TextDirection.ltr,
-      child: InternationalPhoneNumberInput(
-        initialValue: widget.initialValue ?? _phone,
-        onInputChanged: (number) {
-          _phone = number;
-          widget.onChanged?.call(number);
-        },
-        selectorConfig: const SelectorConfig(
-          selectorType: PhoneInputSelectorType.DROPDOWN,
-          showFlags: true,
-          useEmoji: true,
-          leadingPadding: 8,
-          trailingSpace: false,
-        ),
-        ignoreBlank: false,
-        autoValidateMode: AutovalidateMode.disabled,
-        formatInput: true,
-        keyboardType: TextInputType.phone,
-        textAlign: TextAlign.left,
-        textAlignVertical: TextAlignVertical.center,
-        inputDecoration: _decoration(context),
-        selectorTextStyle: const TextStyle(
-          color: Color(0xff2F2B3D),
-          fontSize: 14,
-          fontWeight: FontWeight.w400,
-        ),
-        textStyle: const TextStyle(
-          color: Color(0xff2F2B3D),
-          fontSize: 14,
-          fontWeight: FontWeight.w400,
-        ),
-        countries: const ['SY'],
-        validator: (value) => validatePhoneNumberText(value),
-        isEnabled: widget.enabled,
-        locale: 'ar',
-      ),
-    );
-
-    final field = widget.enabled ? input : AbsorbPointer(child: input);
-
-    if (!widget.showLabel || widget.label == null) {
-      return field;
-    }
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          children: [
-            AppText.bodyMedium(widget.label!, fontWeight: FontWeight.w500),
-            if (widget.isRequired)
-              AppText.bodyMedium(
-                '*',
-                color: context.error,
-                fontWeight: FontWeight.w500,
-              ),
-          ],
-        ),
-        const SizedBox(height: 8),
-        field,
-      ],
     );
   }
 }
