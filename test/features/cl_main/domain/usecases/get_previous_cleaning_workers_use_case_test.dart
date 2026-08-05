d4ce20ac402a52914c250da73cfd44eab807716c
Mustafa Fares <mustaphafares445@gmail.com>
2026-08-05 16:25:29 +0300

    Update previous workers query parameter tests

--- test/features/cl_main/domain/usecases/get_previous_cleaning_workers_use_case_test.dart
+++ test/features/cl_main/domain/usecases/get_previous_cleaning_workers_use_case_test.dart
@@ -2,7 +2,7 @@ import 'package:dllni_user_app/features/cl_main/domain/usecases/get_previous_cle
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('getParams includes property type and service schedule', () {
+  test('getParams includes pagination and service schedule only', () {
     final params = GetPreviousCleaningWorkersParams(
       page: 2,
       perPage: 15,
@@ -15,7 +15,6 @@ void main() {
     expect(params.getParams(), {
       'page': 2,
       'per_page': 15,
-      'propertyType': 'villa',
       'scheduledDate': '2026-08-04',
       'scheduledTime': '09:00',
       'durationHours': 1.5,
@@ -39,11 +38,12 @@ void main() {
     );
   });
 
-  test('getParams includes event_assistance propertyType', () {
+  test('getParams does not send property or neighborhood filters', () {
     final params = GetPreviousCleaningWorkersParams(
       propertyType: 'event_assistance',
     );
 
-    expect(params.getParams()['propertyType'], 'event_assistance');
+    expect(params.getParams().containsKey('propertyType'), isFalse);
+    expect(params.getParams().containsKey('neighborhoodId'), isFalse);
   });
 }
