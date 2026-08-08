eef09e99e5381b879905630948c5d5068a4107f9
AHMADMalbetar <107564019+AHMADMalbetar@users.noreply.github.com>
2026-08-08 16:03:10 +0300

    Add loading state to cleaning images

--- lib/features/cl_main/view/widgets/cl_property_type_card_widget.dart
+++ lib/features/cl_main/view/widgets/cl_property_type_card_widget.dart
@@ -15,13 +15,36 @@ class ClPropertyTypeCardWidget extends StatelessWidget {
   final String icon;
   final ClMainHomeDescriptionArgs args;
 
-  ImageProvider<Object> get _imageProvider {
+  bool get _isNetworkImage {
     final uri = Uri.tryParse(icon);
-    if (uri != null && (uri.scheme == 'http' || uri.scheme == 'https')) {
-      return NetworkImage(icon);
+    return uri != null && (uri.scheme == 'http' || uri.scheme == 'https');
+  }
+
+  Widget _buildImage(BuildContext context) {
+    if (_isNetworkImage) {
+      return AppImage.network(
+        icon,
+        width: context.width,
+        height: 200,
+        fit: BoxFit.cover,
+        errorWidget: Container(
+          color: const Color(0xFFF3F4F6),
+          alignment: Alignment.center,
+          child: const Icon(
+            Icons.image_outlined,
+            color: Color(0xFF9CA3AF),
+            size: 36,
+          ),
+        ),
+      );
     }
 
-    return AssetImage(icon);
+    return AppImage.asset(
+      icon,
+      width: context.width,
+      height: 200,
+      fit: BoxFit.cover,
+    );
   }
 
   @override
@@ -31,40 +54,49 @@ class ClPropertyTypeCardWidget extends StatelessWidget {
       onTap: () {
         context.pushRoute('/clmainhomedescription', arguments: args);
       },
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(image: _imageProvider, fit: BoxFit.cover),
-        ),
-        width: context.width,
-        height: 200,
-        padding: const EdgeInsetsDirectional.symmetric(
-          vertical: 12,
-          horizontal: 24,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: SizedBox(
+          width: context.width,
+          height: 200,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              _buildImage(context),
+              Padding(
+                padding: const EdgeInsetsDirectional.symmetric(
+                  vertical: 12,
+                  horizontal: 24,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsetsDirectional.symmetric(
+                        vertical: 10,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          AppText.bodyMedium(
+                            title,
+                            fontWeight: FontWeight.w700,
+                            textAlign: TextAlign.center,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              padding: const EdgeInsetsDirectional.symmetric(vertical: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  AppText.bodyMedium(
-                    title,
-                    fontWeight: FontWeight.w700,
-                    textAlign: TextAlign.center,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
