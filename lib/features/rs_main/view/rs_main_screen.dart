ff40843f09be66c98e3f8b872ea2e6814765e131
Mustafa Fares <mustaphafares445@gmail.com>
2026-08-11 10:44:32 +0300

    Refresh restaurant header counters on entry

--- lib/features/rs_main/view/rs_main_screen.dart
+++ lib/features/rs_main/view/rs_main_screen.dart
@@ -1,9 +1,11 @@
 import 'package:common_package/common_package.dart';
+import 'package:dllni_user_app/core/auth/auth_gate.dart';
 import 'package:dllni_user_app/core/cart/cart_products_count_cubit.dart';
 import 'package:dllni_user_app/core/di/injection.dart';
 import 'package:dllni_user_app/features/rs_main/view/widgets/rs_bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 
+import '../../profile/domain/usecases/fetch_notifications_use_case.dart';
 import '../../profile/view/manager/bloc/profile_bloc.dart';
 import '../../rs_discover/view/screens/rs_discover_screen.dart';
 import '../../rs_favourite/view/screens/rs_favourite_screen.dart';
@@ -48,7 +50,15 @@ class _RsMainScreenState extends State<RsMainScreen>
     );
 
     profileBloc = widget.args.profileBloc;
-    getIt<CartProductsCountCubit>().fetchCount();
+    if (AuthGate.isAuthenticated) {
+      profileBloc.add(
+        FetchNotificationsEvent(
+          params: FetchNotificationsParams(),
+          isReload: true,
+        ),
+      );
+      getIt<CartProductsCountCubit>().fetchCount();
+    }
   }
 
   List<Widget> get pages => [
