56c596f4bdcd2219f43e7c8823d8bba6d8cf090c
AHMADMalbetar <107564019+AHMADMalbetar@users.noreply.github.com>
2026-08-15 22:10:22 +0300

    Restore restaurant home engagement cards on dev

--- lib/features/rs_home/view/widgets/exclusive_offers_section.dart
+++ lib/features/rs_home/view/widgets/exclusive_offers_section.dart
@@ -9,6 +9,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../../core/widgets/rs_app_offer_card.dart';
+import '../../../../generated/assets.dart';
 import '../../data/models/fetch_restaurant_home_exclusive_offers_model.dart';
 
 class ExclusiveOffersSection extends StatelessWidget {
@@ -124,6 +125,158 @@ class ExclusiveOffersSection extends StatelessWidget {
               );
             }
           },
+        ),
+        SizedBox(height: 24),
+        _RestaurantHomeEngagementCards(),
+      ],
+    );
+  }
+}
+
+class _RestaurantHomeEngagementCards extends StatelessWidget {
+  const _RestaurantHomeEngagementCards();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            AppText(
+              'عروض حصرية بالقرب منك',
+              style: TextStyle(
+                color: Color(0xFF1A1A1A),
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+                height: 24 / 16,
+              ),
+            ),
+            SizedBox(width: 8),
+            FaIcon(
+              FontAwesomeIcons.solidCircleCheck,
+              color: context.primaryContainer,
+              size: 14,
+            ),
+          ],
+        ),
+        SizedBox(height: 16),
+        Row(
+          spacing: 11,
+          children: [
+            Expanded(
+              child: InkWell(
+                borderRadius: BorderRadius.circular(24),
+                onTap: () => context.pushRoute('/luckyboxsetup'),
+                child: Container(
+                  height: 153,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(24),
+                    gradient: LinearGradient(
+                      colors: [Color(0xffFF7A00), Color(0xff994900)],
+                      end: Alignment.bottomRight,
+                      begin: Alignment.topLeft,
+                    ),
+                  ),
+                  padding: EdgeInsetsDirectional.symmetric(
+                    horizontal: 14,
+                    vertical: 16,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          AppImage.asset(Assets.images.giftImage.path, height: 88),
+                        ],
+                      ),
+                      AppText.bodyLarge(
+                        'صندوق الحظ',
+                        fontWeight: FontWeight.bold,
+                        color: context.onPrimary,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: InkWell(
+                borderRadius: BorderRadius.circular(24),
+                onTap: () => context.pushRoute('/ordervoting'),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(24),
+                    gradient: LinearGradient(
+                      colors: [Color(0xff384EDE), Color(0xff1E2A78)],
+                      begin: AlignmentGeometry.topLeft,
+                      end: AlignmentGeometry.bottomRight,
+                    ),
+                  ),
+                  padding: EdgeInsetsDirectional.symmetric(
+                    horizontal: 14,
+                    vertical: 16,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          AppImage.asset(
+                            Assets.images.threeStarsImage.path,
+                            height: 88,
+                          ),
+                        ],
+                      ),
+                      AppText.bodyLarge(
+                        'التصويت',
+                        fontWeight: FontWeight.bold,
+                        color: context.onPrimary,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 16),
+        InkWell(
+          borderRadius: BorderRadius.circular(24),
+          onTap: () => context.pushRoute('/group-order/create'),
+          child: Container(
+            height: 153,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24),
+              gradient: LinearGradient(
+                colors: [Color(0xff2EC4B6), Color(0xff165E57)],
+                begin: AlignmentGeometry.topLeft,
+                end: AlignmentGeometry.bottomRight,
+              ),
+            ),
+            padding: EdgeInsetsDirectional.symmetric(horizontal: 14, vertical: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    AppImage.asset(Assets.images.socialImage.path, height: 88),
+                  ],
+                ),
+                AppText.bodyLarge(
+                  'التكامل الاجتماعي',
+                  fontWeight: FontWeight.bold,
+                  color: context.onPrimary,
+                ),
+              ],
+            ),
+          ),
         ),
       ],
     );
